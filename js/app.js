f9368c8662a7a62e7abc4c85cc1df8a5e225703d
Diara-Bamba <diarabamba@gmail.com>
2018-06-13 17:43:56 -0400

    Set up Event listener for when a card is clicked

--- js/app.js
+++ js/app.js
@@ -5,7 +5,9 @@ console.log(cards);
 
 // Card deck of all the cards in the game.
 const deck = document.getElementById("card-deck");
+
 /*
+
  * Display the cards on the page
  *   - shuffle the list of cards using the provided "shuffle" method below
  *   - loop through each card and create its HTML
@@ -26,11 +28,18 @@ function shuffle(array) {
 
     return array;
 }
+// Shuffles the cards when page is refreshed.
+document.body.onload = startGame();
 
+function startGame() {
+	cards= shuffle(cards);
 
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
+}
+
+// Event listener for when a card is clicked:
+ document.querySelectorAll(".card").addEventListener("click", displayCard);
+
+ /*  - display the card's symbol (put this functionality in another function that you call from this one)
  *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
  *  - if the list already has another card, check to see if the two cards match
  *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
