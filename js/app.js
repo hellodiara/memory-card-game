ddfba35f1de57a936d6a41c8d3e07cdd6a62a523
Diara-Bamba <diarabamba@gmail.com>
2018-06-20 17:51:54 -0400

    Restarted: Display the card's symbol when clicked

--- js/app.js
+++ js/app.js
@@ -1,31 +1,4 @@
-// Card array that holds all cards
-let card = document.getElementsByClassName("card");
-let cards = [...card]
-console.log(cards);
 
-// Card deck of all the cards in the game.
-const deck = document.getElementById("card-deck");
-
-// Moves
-let moves = 0;
-let counter = document.querySelector(".moves");
-
-// Matched Cards variable
-let matchedCard = document.getElementsByClassName("match");
-
-// Star icons variable
-const stars = document.querySelectorAll(".fa-star");
-
-// List of stars variable
- let listOfStars = document.querySelectorAll(".stars li");
-
-/*
-
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
- */
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
@@ -41,24 +14,6 @@ function shuffle(array) {
 
     return array;
 }
-// Shuffles the cards when page is refreshed.
-document.body.onload = startGame();
-
-function startGame() {
-	cards= shuffle(cards);
-
-}
-
-// Display Cards
-var showCard = function () {
-	this.classList.toggle("open");
-	this.classList.toggle("show");
-	this.classList.toggle("disabled");
-};
-// Event listener for when a card is clicked:
- document.querySelectorAll(".card").addEventListener("click", showCard);
- document.querySelectorAll(".card").addEventListener("click", openCard);
- document.querySelectorAll(".card").addEventListener("click", congrats);
 
  /*  - display the card's symbol (put this functionality in another function that you call from this one)
  *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
@@ -68,3 +23,12 @@ var showCard = function () {
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
+
+//Display the card's symbol when clicked
+let allCards = document.querySelectorAll('.card');
+
+allCards.forEach(function(card) {
+    card.addEventListener('click' , function(e){
+        card.classList.add('open' , 'show');
+    });
+ });
