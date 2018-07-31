ce9335bd008410537b21aa81d47166530752a5cc
Diara-Bamba <diarabamba@gmail.com>
2018-07-31 17:08:20 -0400

    Clock functionality

--- js/app.js
+++ js/app.js
@@ -2,6 +2,11 @@
 const deck = document.querySelector('.deck');
 let toggleCards = []; // store all cards in an array
 let moves = 0;
+
+// Global variables for clock
+let clockOff = true;
+let time = 0;
+let clockId;
 /*
  * Display the cards on the page
  *   - shuffle the list of cards using the provided "shuffle" method below
@@ -46,8 +51,11 @@ function shuffle(array) {
 
  deck.addEventListener('click', event => {
     const clickTarget = event.target;
-    if (isMoveValid(clickTarget
-        )) {
+    if (isMoveValid(clickTarget)) {
+        if (clockOff) {
+            startClock();
+            clockOff = false;
+        }
         toggleCard(clickTarget);
         addToggleCard(clickTarget);
         if (toggleCards.length === 2) {
@@ -114,3 +122,27 @@ function hideStar() {
         }
     }
 }
+// Clock functionality
+function startClock() {
+    clockId = setInterval(() => {
+        time++;
+        displayTime();
+        console.log(time);
+    }, 1000);
+}
+// stops the clock
+function stopClock() {
+    clearInterval(clockId);
+}
+// Displays the current time in the scoreboard
+function displayTime() {
+    const clock = document.querySelector('.clock');
+    const minutes = Math.floor(time / 60);
+    const seconds = time % 60;
+    if (seconds < 10) {
+        clock.innerHTML = `${minutes}:0${seconds}`;
+    } else {
+        clock.innerHTML = `${minutes}:${seconds}`;
+    }
+}
+
